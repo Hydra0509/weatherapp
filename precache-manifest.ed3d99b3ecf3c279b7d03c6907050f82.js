49254b8b16965012977cf6b2bbd78cdee6a961f1
Kovács Attila <giga05091@hotmail.com>
2020-05-26 22:49:57 +0200

    Updates

---
+++ precache-manifest.ed3d99b3ecf3c279b7d03c6907050f82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fda226f6861f91ea5544b0a76cdbce",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "1d77b19fb0b83b3363c1",
+    "url": "/weatherapp/static/css/main.b0e270bc.chunk.css"
+  },
+  {
+    "revision": "5614467a4f5e60e40083",
+    "url": "/weatherapp/static/js/2.b45a3554.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.b45a3554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d77b19fb0b83b3363c1",
+    "url": "/weatherapp/static/js/main.5fa15138.chunk.js"
+  },
+  {
+    "revision": "26373162a9e6f2118022",
+    "url": "/weatherapp/static/js/runtime-main.d5bba109.js"
+  },
+  {
+    "revision": "5f2f0c427d5527c346d0dea867bcc16b",
+    "url": "/weatherapp/static/media/clear-weather.5f2f0c42.jpg"
+  },
+  {
+    "revision": "377e104b6d3a4856afb510f4bea58c55",
+    "url": "/weatherapp/static/media/cold-weather.377e104b.jpg"
+  },
+  {
+    "revision": "0195d449cb3b5f418f05eed5d77af8b2",
+    "url": "/weatherapp/static/media/rain-weather.0195d449.jpg"
+  },
+  {
+    "revision": "87f7df48fdf211ac797b43c2cca4fd2d",
+    "url": "/weatherapp/static/media/warm-weather.87f7df48.jpg"
+  }
+]);
